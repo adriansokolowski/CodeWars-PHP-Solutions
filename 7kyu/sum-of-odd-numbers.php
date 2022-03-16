653e65041e11cf99ff4e5fd2aedc226526d8f22c
Adrian Sokołowski <37288976+adriansokolowski@users.noreply.github.com>
2022-03-16 15:42:50 +0100

    Create sum-of-odd-numbers.php

---
+++ 7kyu/sum-of-odd-numbers.php
@@ -0,0 +1,33 @@
+<?php
+/*
+Description:
+Given the triangle of consecutive odd numbers:
+
+             1
+          3     5
+       7     9    11
+   13    15    17    19
+21    23    25    27    29
+...
+Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.: (Input --> Output)
+
+1 -->  1
+2 --> 3 + 5 = 8
+*/
+
+// Solution 1
+function rowSumOddNumbers($n) {
+  return pow($n, 3);
+}
+
+// Solution 2
+function rowSumOddNumbers($n) {
+  return $n**3;
+}
+
+// Solution 3
+function rowSumOddNumbers($n) {
+  return $n * $n * $n;
+}
+
+?>
